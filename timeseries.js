49415f15bf3d32ed7d3e020d75c199faa27981be
Tor Nordqvist <tor.nordqvist@captor.se>
2016-12-09 00:15:46 +0100

    centralWeightedStdev & toTrace added

--- timeseries.js
+++ timeseries.js
@@ -333,7 +333,9 @@ var TimeSeries = (function () {
         return res;
     };
     TimeSeries.prototype.dateToString = function (d) {
-        return d.toISOString().substr(0, 10);
+        d = new Date(d.getTime());
+        d.setMinutes(d.getMinutes() - d.getTimezoneOffset());
+        return d.toISOString().substring(0, 10);
     };
     TimeSeries.prototype.bondReturn = function (d0, d1, maturity, yearlyCoupons) {
         var dt = (d1.timestamp.getTime() - d0.timestamp.getTime()) / this.ticksPerYear;
@@ -399,7 +401,120 @@ var TimeSeries = (function () {
             ts.push(this.items[endindex].clone);
         }
         var res = new TimeSeries(ts);
-        res.name = 'MAX DRAWDOWN';
+        res.name = this.name;
+        return res;
+    };
+    TimeSeries.prototype.smoother = function (period) {
+        if (period === undefined)
+            period = 1;
+        var sum = 0;
+        var res = this.clone;
+        for (var j = 0; j < period; j++) {
+            for (var i = 3; i < this.count; i++) {
+                res.items[i - 1].value = (res.items[i - 2].value + res.items[i].value) / 2;
+            }
+        }
+        return res;
+    };
+    TimeSeries.prototype.erf = function (x) {
+        var cof = [-1.3026537197817094, 6.4196979235649026e-1, 1.9476473204185836e-2,
+            -9.561514786808631e-3, -9.46595344482036e-4, 3.66839497852761e-4,
+            4.2523324806907e-5, -2.0278578112534e-5, -1.624290004647e-6,
+            1.303655835580e-6, 1.5626441722e-8, -8.5238095915e-8,
+            6.529054439e-9, 5.059343495e-9, -9.91364156e-10,
+            -2.27365122e-10, 9.6467911e-11, 2.394038e-12,
+            -6.886027e-12, 8.94487e-13, 3.13092e-13,
+            -1.12708e-13, 3.81e-16, 7.106e-15,
+            -1.523e-15, -9.4e-17, 1.21e-16,
+            -2.8e-17];
+        var j = cof.length - 1;
+        var isneg = false;
+        var d = 0;
+        var dd = 0;
+        var t, ty, tmp, res;
+        if (x < 0) {
+            x = -x;
+            isneg = true;
+        }
+        t = 2 / (2 + x);
+        ty = 4 * t - 2;
+        for (; j > 0; j--) {
+            tmp = d;
+            d = ty * d - dd + cof[j];
+            dd = tmp;
+        }
+        res = t * Math.exp(-x * x + 0.5 * (cof[0] + ty * d) - dd);
+        return isneg ? res - 1 : 1 - res;
+    };
+    TimeSeries.prototype.erfc = function (x) {
+        return 1 - this.erf(x);
+    };
+    TimeSeries.prototype.erfcinv = function (p) {
+        var j = 0;
+        var x, err, t, pp;
+        if (p >= 2)
+            return -100;
+        if (p <= 0)
+            return 100;
+        pp = (p < 1) ? p : 2 - p;
+        t = Math.sqrt(-2 * Math.log(pp / 2));
+        x = -0.70711 * ((2.30753 + t * 0.27061) /
+            (1 + t * (0.99229 + t * 0.04481)) - t);
+        for (; j < 2; j++) {
+            err = this.erfc(x) - pp;
+            x += err / (1.12837916709551257 * Math.exp(-x * x) - x * err);
+        }
+        return (p < 1) ? x : -x;
+    };
+    TimeSeries.prototype.normalInv = function (p, mean, std) {
+        return -1.41421356237309505 * std * this.erfcinv(2 * p) + mean;
+    };
+    TimeSeries.prototype.centralWeightedStdev = function () {
+        var vs = this.values;
+        vs.sort(function (d1, d2) { return d1 - d2; });
+        var swx = 0.0;
+        var swx2 = 0.0;
+        var sw = 0.0;
+        var swy = 0.0;
+        var swxy = 0.0;
+        var n = vs.length;
+        for (var i = 0; i < n; i++) {
+            var u = (i + 0.5) / n;
+            var x = this.normalInv(u, 0, 1);
+            var w = (1 + Math.cos(2 * Math.PI * (u - 0.5))) / 2;
+            var y = vs[i];
+            var xw = x * w;
+            swx += xw;
+            swx2 += xw * x;
+            sw += w;
+            swy += w * y;
+            swxy += xw * y;
+        }
+        var a = swx * swx - swx2 * sw;
+        var stdev = (swy * swx - swxy * sw) / a;
+        return stdev;
+    };
+    TimeSeries.prototype.toTrace = function (color, props) {
+        var _this = this;
+        var res = {
+            x: this.items.map(function (d) { return _this.timestampFormatFun(d.timestamp); }),
+            y: this.items.map(function (d) { return _this.valueFormatFun(d.value); }),
+            type: 'scatter',
+            mode: 'lines',
+            marker: {
+                color: color,
+                size: 8,
+                line: { width: 0 }
+            },
+            line: { width: 2.5 }
+        };
+        if (props !== undefined) {
+            var keys = Object.keys(props);
+            for (var i = 0; i < keys.length; i++) {
+                var k = keys[i];
+                res[k] = props[k];
+            }
+        }
         return res;
     };
     TimeSeries.prototype.renderTable = function (d3, selector) {
